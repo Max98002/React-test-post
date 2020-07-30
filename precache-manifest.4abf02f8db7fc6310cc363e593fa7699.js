e9fc2146148da555c878af9328dc425d03711c58
maksimka98002@gmail.com <bilanych98002@MacBook-Pro-Maksim.local>
2020-07-30 13:25:09 +0300

    Updates

---
+++ precache-manifest.4abf02f8db7fc6310cc363e593fa7699.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e778386f1388a401ed71e1cfcb046de",
+    "url": "/React-test-post/index.html"
+  },
+  {
+    "revision": "c42998069159b9f1a138",
+    "url": "/React-test-post/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "67bd31012b44cb5f4173",
+    "url": "/React-test-post/static/css/main.a33fbc22.chunk.css"
+  },
+  {
+    "revision": "c42998069159b9f1a138",
+    "url": "/React-test-post/static/js/2.748971ff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-test-post/static/js/2.748971ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67bd31012b44cb5f4173",
+    "url": "/React-test-post/static/js/main.319aa4f4.chunk.js"
+  },
+  {
+    "revision": "49cbacd356c2196b996e",
+    "url": "/React-test-post/static/js/runtime-main.472d1e70.js"
+  }
+]);
